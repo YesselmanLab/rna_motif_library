c2b3e2460d049cc97b915fcd76a0f4e1cf0d61b5
f-ritz <dabeast24680@gmail.com>
2024-07-03 15:39:16 -0500

    updated setup and requirements files

--- setup.py
+++ setup.py
@@ -1,43 +1,44 @@
 from setuptools import setup
 
 setup(
-        # metadata
-        name='rna_motif_library',
-        version='1.0.0',
-        description='a minimal package for rna_motif_library creation',
-        long_description_content_type="test/markdown",
-        author='Joe Yesselman',
-        author_email='jyesselm@unl.edu',
-        url='https://github.com/jyesselm/rna_motif_library',
-        # package contents
-        packages=[
-            'rna_motif_library',
-        ],
-        package_dir={'rna_motif_library': 'rna_motif_library'},
-        py_modules=[
-            'rna_motif_library/dssr',
-            'rna_motif_library/dssr_hbonds',
-            'rna_motif_library/figure_plotting',
-            'rna_motif_library/settings',
-            'rna_motif_library/snap',
-            'rna_motif_library/tertiary_contacts',
-            'rna_motif_library/update_library',
-            'rna_motif_library/update_library_nmr'
-        ],
-        include_package_data=True,
-        install_requires=[
-            'pandas',
-        ],
-        zip_safe=False,
-        keywords='rna_motif_library',
-        classifiers=[
-            'Intended Audience :: Developers',
-            'Natural Language :: English',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: Implementation :: PyPy',
-        ],
-        entry_points={
-            'console_scripts': [
-            ]
-        }
+    name='rna_motif_library',
+    version='1.0.0',
+    description='A minimal package for RNA motif library creation',
+    long_description_content_type="text/markdown",
+    author='Joe Yesselman',
+    author_email='jyesselm@unl.edu',
+    url='https://github.com/jyesselm/rna_motif_library',
+    packages=['rna_motif_library'],
+    package_dir={'rna_motif_library': 'rna_motif_library'},
+    py_modules=[
+        'rna_motif_library.dssr',
+        'rna_motif_library.dssr_hbonds',
+        'rna_motif_library.figure_plotting',
+        'rna_motif_library.settings',
+        'rna_motif_library.snap',
+        'rna_motif_library.tertiary_contacts',
+        'rna_motif_library.update_library',
+        'rna_motif_library.update_library_nmr'
+    ],
+    include_package_data=True,
+    install_requires=[
+        'pydssr~=0.0.3',
+        'biopandas~=0.4.1',
+        'pandas~=1.5.3',
+        'wget~=3.2',
+        'setuptools~=60.2.0',
+        'requests~=2.28.2',
+        'numpy~=1.24.2',
+        'seaborn~=0.13.1',
+        'matplotlib~=3.7.1'
+    ],
+    zip_safe=False,
+    keywords='RNA motif library',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    entry_points={}
 )
