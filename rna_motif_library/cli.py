a087eaf27b8d60fb5f434ec943763f7f72a0cd40
Joseph Yesselman <jyesselm@unl.edu>
2024-09-14 17:23:19 -0500

    fixed function timing with decorator

--- rna_motif_library/cli.py
+++ rna_motif_library/cli.py
@@ -2,7 +2,9 @@ import time
 import warnings
 import os
 import click
-
+import sys
+import functools
+import pandas as pd
 from rna_motif_library.settings import LIB_PATH
 from rna_motif_library.logger import setup_logging, get_logger
 from update_library import (
@@ -16,6 +18,38 @@ from update_library import (
 log = get_logger("cli")
 
 
+def log_and_setup(func):
+    """
+    Decorator to set up logging and log the start and end of the function execution.
+
+    Args:
+        func (callable): The function to wrap.
+
+    Returns:
+        callable: The wrapped function with logging and setup.
+    """
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        func_name = func.__name__
+        setup_logging()
+        log.info("Ran at commandline as: %s", " ".join(sys.argv))
+        start_time = time.time()
+        log.info("Starting time: %s" % pd.Timestamp.now())
+        try:
+            result = func(*args, **kwargs)
+        finally:
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            formatted_time = time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
+            log.info(
+                f"{func_name} execution completed. Total runtime: {formatted_time}"
+            )
+        return result
+
+    return wrapper
+
+
 @click.group()
 def cli():
     pass
@@ -23,6 +57,7 @@ def cli():
 
 @cli.command()
 @click.option("--threads", default=1, help="Number of threads to use.")
+@log_and_setup
 def download_cifs(threads):
     """
     Downloads CIFs specified in the CSV from the RCSB PDB database.
@@ -34,25 +69,20 @@ def download_cifs(threads):
         None
 
     """
-    setup_logging()
     warnings.filterwarnings("ignore")
-    start_time = time.time()
     csv_directory = os.path.join(LIB_PATH, "data/csvs/")
     csv_files = [file for file in os.listdir(csv_directory) if file.endswith(".csv")]
+    # TODO specify which CSV to use
+    if len(csv_files) == 0:
+        log.error(f"No CSV files found in directory: {csv_directory}")
+        return
     csv_path = os.path.join(csv_directory, csv_files[0])
     download_cif_files(csv_path, threads)
-    end_time = time.time()
-    total_seconds = int(end_time - start_time)
-    hours = total_seconds // 3600
-    minutes = (total_seconds % 3600) // 60
-    seconds = total_seconds % 60
-    log.info(f"Download started at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time))}")
-    log.info(f"Download finished at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(end_time))}")
-    log.info(f"Time taken: {hours} hours, {minutes} minutes, {seconds} seconds")
 
 
 @cli.command()
 @click.option("--threads", default=1, help="Number of threads to use.")
+@log_and_setup
 def process_dssr(threads):
     """
     Processes every downloaded PDB with DSSR, extracting the secondary structure into a JSON.
@@ -64,22 +94,13 @@ def process_dssr(threads):
         None
 
     """
-    setup_logging()
     warnings.filterwarnings("ignore")
-    start_time = time.time()
     get_dssr_files(threads)
-    end_time = time.time()
-    total_seconds = int(end_time - start_time)
-    hours = total_seconds // 3600
-    minutes = (total_seconds % 3600) // 60
-    seconds = total_seconds % 60
-    log.info(f"DSSR processing started at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time))}")
-    log.info(f"DSSR processing finished at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(end_time))}")
-    log.info(f"Time taken: {hours} hours, {minutes} minutes, {seconds} seconds")
 
 
 @cli.command()
 @click.option("--threads", default=1, help="Number of threads to use.")
+@log_and_setup
 def process_snap(threads):
     """
     Processes every downloaded PDB with SNAP, extracting RNA-protein interaction data.
@@ -91,21 +112,11 @@ def process_snap(threads):
         None
 
     """
-    setup_logging()
     warnings.filterwarnings("ignore")
-    start_time = time.time()
     get_snap_files(threads)
-    end_time = time.time()
-    total_seconds = int(end_time - start_time)
-    hours = total_seconds // 3600
-    minutes = (total_seconds % 3600) // 60
-    seconds = total_seconds % 60
-    log.info(f"SNAP processing started at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time))}")
-    log.info(f"SNAP processing finished at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(end_time))}")
-    log.info(f"Time taken: {hours} hours, {minutes} minutes, {seconds} seconds")
 
 
-@cli.command()  # Set command name
+@cli.command()
 @click.option(
     "--limit", default=None, type=int, help="Limit the number of PDB files processed."
 )
@@ -115,6 +126,7 @@ def process_snap(threads):
     type=str,
     help="Process a specific PDB within the set, without extensions",
 )
+@log_and_setup
 def generate_motifs(limit, pdb):
     """
     Extracts motifs from source PDB using data from DSSR, and interactions using data from DSSR and SNAP.
@@ -127,24 +139,12 @@ def generate_motifs(limit, pdb):
         None
 
     """
-    setup_logging()
     warnings.filterwarnings("ignore")
-    start_time = time.time()
-
     generate_motif_files(limit, pdb)
 
-    end_time = time.time()
-    total_seconds = int(end_time - start_time)
-    hours = total_seconds // 3600
-    minutes = (total_seconds % 3600) // 60
-    seconds = total_seconds % 60
 
-    log.info(f"Motif generation started at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time))}")
-    log.info(f"Motif generation finished at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(end_time))}")
-    log.info(f"Time taken: {hours} hours, {minutes} minutes, {seconds} seconds")
-
-
-@cli.command()  # Set command name
+@cli.command()
+@log_and_setup
 def load_tertiary_contacts():
     """
     Finds tertiary contacts using hydrogen bonding data.
@@ -156,19 +156,8 @@ def load_tertiary_contacts():
         None
 
     """
-    setup_logging()
     warnings.filterwarnings("ignore")
-    start_time = time.time()
     find_tertiary_contacts()
-    end_time = time.time()
-    total_seconds = int(end_time - start_time)
-    hours = total_seconds // 3600
-    minutes = (total_seconds % 3600) // 60
-    seconds = total_seconds % 60
-
-    log.info(f"Tertiary contact discovery started at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time))}")
-    log.info(f"Tertiary contact discovery finished at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(end_time))}")
-    log.info(f"Time taken: {hours} hours, {minutes} minutes, {seconds} seconds")
 
 
 if __name__ == "__main__":
